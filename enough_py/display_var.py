6cc5fc6349465e0ad59a45c65d6f5b4b348e910b
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-20 19:29:04 +0800

    Creating and Displaying Variables

---
+++ enough_py/display_var.py
@@ -0,0 +1,6 @@
+first_name = "Kevin"
+last_name = "Bacon"
+age = 59
+birth_date = "07/08/1958"
+
+print("My name is %s %s.\nI was born on %s and I'm %s years old." % (first_name, last_name, birth_date, age))
