d554cf5081f42f4970a5b6073fc946100446b8e5
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-20 23:54:56 +0800

    Standard Library: re & math

--- script_py/shutil_glob.py
+++ script_py/shutil_glob.py
@@ -1,33 +1,35 @@
+# python .\script_py\rand_json.py
 import glob
 import os
 import shutil
 import json
+import re
 
 try:
     os.mkdir("./processed")
 except OSError:
     print("'processed' directory already exist")
+    shutil.rmtree('./processed')
+    os.mkdir("./processed")
+finally:
+    # Get a list of recipts
+    # receipts = glob.glob('./receipt/receipt-[0-9]*.json')
+    receipts = [f for f in glob.iglob('./receipt/receipt-[0-9]*.json') if re.match('./receipt\\\\receipt-[0-9][02468].json', f)]
+    print(receipts)
+    subtotal = 0.0
 
-# Get a list of recipts
-receipts = glob.glob('./receipt/receipt-[0-9]*.json')
+    # Iterate over the receipts
+    for path in receipts:
+        # read content and tally a subtotal
+        with open(path) as f:
+            content = json.load(f)
+            subtotal += float(content['value'])
+        destination = path.replace('receipt', 'processed')
+        print(destination)
+        # mv the file to the processed directory
+        shutil.move(path, destination)
+        # print that we processed the file
+        print("moved '%s' to '%s'" % (path, destination))
 
-subtotal = 0.0
-
-for path in receipts:
-    with open(path) as f:
-        content = json.load(f)
-        subtotal += float(content['value'])
-    name = path.split('\\')[-1]
-    destination = "./processed/%s" % name
-    print(destination)
-    shutil.move(path, destination)
-    print("moved '%s' to '%s'" % (path, destination))
-
-print("Receipt subtotal: $%.2f" % subtotal)
-
-# Iterate over the receipts
-# read content and tally a subtotal
-# mv the file to the processed directory
-# print that we processed the file
-
-# Print the subtotal of all processed receipts
+    # Print the subtotal of all processed receipts
+    print("Receipt subtotal: $%s" % round(subtotal, 2))
