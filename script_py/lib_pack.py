941493245955036741239e165cbd1da6f09c84fc
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-20 20:59:30 +0800

    Using Standard Library Packages

---
+++ script_py/lib_pack.py
@@ -0,0 +1,13 @@
+from time import localtime, strftime, mktime
+
+start_time = localtime()
+print("Timer start at %s" % strftime("%X", start_time))
+
+# Wait for user input
+input("Please press Enter to continue...")
+
+stop_time = localtime()
+difference = mktime(stop_time) - mktime(start_time)
+
+print("Timer stopped at %s" % strftime("%X", stop_time))
+print("Total time: %s seconds" % difference)
