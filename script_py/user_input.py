3edc00890f504698ca56933766be8cac30ef93c5
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-20 20:15:00 +0800

    Reading User Input

---
+++ script_py/user_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+birthdate = input("What is your birthdate? ")
+
+age = int(input("How old are you? "))
+
+print("%s was born on %s" % (name, birthdate))
+print("Half of your age is %s" % (age / 2))
