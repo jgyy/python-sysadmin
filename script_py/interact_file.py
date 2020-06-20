ef026066f5dfe1d6ca72d00d01dbcf80d9b4b16e
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2020-06-20 21:39:06 +0800

    updated the xmen files

--- script_py/interact_file.py
+++ script_py/interact_file.py
@@ -1,5 +1,5 @@
-with open('xmen.txt', 'a') as xmen_file:
-    xmen_file.write("Professor Xavier\n")
+with open('./script_py/xmen.txt', 'a') as xmen_file:
+    xmen_file.write("\nProfessor Xavier\n")
 
-with open('xmen.txt') as xmen_file:
+with open('./script_py/xmen.txt') as xmen_file:
     print(xmen_file.read())
